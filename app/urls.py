1bba60c7831d372b26188fb72dc2d79a9e29b9b6
Rémy <remdub@live.fr>
2017-06-30 10:15:48 +0200

    Add proposal update page, display service lines in proposal detail

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     url(r'^list/$', ProposalListView.as_view(), name='proposal-list'),
+    url(r'^create/', ProposalCreateView.as_view(), name='proposal_create'),
     url(r'^details/(?P<pk>\d+)/$', ProposalDetailView.as_view(), name='proposal_detail'),
-    url(r'^create/', ProposalCreateView.as_view(), name='proposal_create')
+    url(r'^details/(?P<pk>\d+)/edit/$', ProposalUpdateView.as_view(), name='proposal_update'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, reverse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.db import transaction
+
 from .models import *
 from .forms import ProposalInlineFormSet
 
@@ -42,6 +44,38 @@ class ProposalCreateView(CreateView):
             lines.save(form)
 
         return super(ProposalCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('proposal_detail', kwargs={'pk' : self.object.id})
+
+class ProposalUpdateView(UpdateView):
+    model = Proposal
+    fields = "__all__"
+
+    def line(self):
+        if self.request.POST:
+            return ProposalInlineFormSet(self.request.POST)
+        else:
+            return ProposalInlineFormSet(instance=self.object)
+
+    def get_context_data(self, **kwargs):
+        context = super(ProposalUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['serviceline'] = ProposalInlineFormSet(self.request.POST, instance=self.object)
+        else:
+            context['serviceline'] = ProposalInlineFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        serviceline = context['serviceline']
+        with transaction.atomic():
+            self.object = form.save()
+        if serviceline.is_valid():
+            serviceline.instance = self.object
+            serviceline.save()
+        return super(ProposalUpdateView, self).form_valid(form)
+
 
     def get_success_url(self):
         return reverse('proposal_detail', kwargs={'pk' : self.object.id})
